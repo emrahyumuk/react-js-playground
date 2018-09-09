ae38bf9b52ee9df2ed84c18bcdf43ee7b9dcc2d7
mRA <mra@mRA.Home.PC>
2018-09-09 21:03:56 +0300

    react-complete-guide: css updated

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -37,7 +37,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -62,11 +63,20 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+    }
+
+    let classes = [];
+    if (this.state.persons.length <= 0) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
-        <h1>Hey</h1>
+        <h1 className={classes.join(' ')}>Hey</h1>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
