7a5529f79f9b3cff641ae38fd57195613fb868bc
mRA <mra@mRA.Home.PC>
2018-11-05 08:09:00 +0300

    the-burger-builder/3-burger-builder-routing: orders

--- the-burger-builder/3-burger-builder-routing/src/containers/Orders/Orders.js
+++ the-burger-builder/3-burger-builder-routing/src/containers/Orders/Orders.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
   state = {
@@ -12,7 +13,15 @@ class Orders extends Component {
     axios
       .get('/orders.json')
       .then(res => {
-        this.setState({ orders: res.data, loading: false });
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        console.log(fetchedOrders);
+        this.setState({ orders: fetchedOrders, loading: false });
       })
       .catch(err => {
         this.setState({ loading: false });
@@ -21,11 +30,16 @@ class Orders extends Component {
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
+        ))}
       </div>
     );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
