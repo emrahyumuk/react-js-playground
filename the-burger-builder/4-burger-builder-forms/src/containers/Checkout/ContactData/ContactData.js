8374c74cffc456bbe5a6fcb216dde879f968e3ff
Emrah Yumuk <emrahyumuk@gmail.com>
2018-11-19 17:40:02 +0300

    the-burger-builder:4-burger-builder-forms: validations

--- the-burger-builder/4-burger-builder-forms/src/containers/Checkout/ContactData/ContactData.js
+++ the-burger-builder/4-burger-builder-forms/src/containers/Checkout/ContactData/ContactData.js
@@ -12,27 +12,42 @@ class ContactData extends Component {
       name: {
         elementType: 'input',
         elementConfig: { type: 'text', placeholder: 'Your Name' },
-        value: ''
+        value: '',
+        validation: { required: true },
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
         elementConfig: { type: 'text', placeholder: 'Street' },
-        value: ''
+        value: '',
+        validation: { required: true },
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
         elementConfig: { type: 'text', placeholder: 'ZIP Code' },
-        value: ''
+        value: '',
+        validation: { required: true, minLength: 5, maxLength: 5 },
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
         elementConfig: { type: 'text', placeholder: 'Country' },
-        value: ''
+        value: '',
+        validation: { required: true },
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
         elementConfig: { type: 'text', placeholder: 'Your E-mail' },
-        value: ''
+        value: '',
+        validation: { required: true },
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -42,9 +57,12 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' }
           ]
         },
-        value: ''
+        value: '',
+        validation: {},
+        valid: true
       }
     },
+    formIsValid: false,
     loading: false
   };
 
@@ -74,12 +92,38 @@ class ContactData extends Component {
       });
   };
 
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+    if (rules.requried) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
   inputChangeHandler = (event, inputIdentifier) => {
     const updatedOrderForm = { ...this.state.orderForm };
     const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    this.setState({ orderForm: updatedOrderForm });
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   };
 
   render() {
@@ -98,10 +142,17 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={event => this.inputChangeHandler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success" clicked={this.orderHandler}>
+        <Button
+          btnType="Success"
+          clicked={this.orderHandler}
+          disabled={!this.state.formIsValid}
+        >
           ORDER
         </Button>
       </form>
